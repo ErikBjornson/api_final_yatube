7bfaebf7cbf1811ae9e6f07d267cdbd148ca96b4
ErikBjornson <newpochtushka@gmail.com>
2025-05-04 13:29:00 +0300

    fix: fixed permissions

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -7,4 +7,4 @@ class IsAuthenticatedUserOrAnonimous(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return False
+        return obj.author == request.user
